4a659b0f29ccd15c4113d8e6a8b03aa655416910
dan21220022 <dan21220022@gmail.com>
2022-01-20 10:52:27 +0200

    added OsUtils

---
+++ src/main/java/OsUtils.java
@@ -0,0 +1,18 @@
+public final class OsUtils
+{
+    private static String OS = null;
+    public static String getOsName()
+    {
+        if(OS == null) { OS = System.getProperty("os.name"); }
+        return OS;
+    }
+    public static boolean isWindows()
+    {
+        return getOsName().startsWith("Windows");
+    }
+
+    public static boolean isUnix()
+    {
+        return getOsName().contains("nix") || getOsName().contains("nux") || getOsName().contains("aix");
+    }
+}
