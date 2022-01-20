6a910d54139d878e354c78156006b826e9af7538
dan21220022 <dan21220022@gmail.com>
2022-01-20 08:19:56 +0200

    added stringToRecord

--- src/main/java/JsonConverter.java
+++ src/main/java/JsonConverter.java
@@ -1,5 +1,20 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+
 public class JsonConverter {
 
-    
+    public HashMap<String, Object> stringToRecord(String jsonString)
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String,Object> map = new HashMap<>();
+        try {
+            map = mapper.readValue(jsonString, HashMap.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 
 }
